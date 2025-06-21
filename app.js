c4e22d7e23d2e3e4831ed80dbdc1a39dba1543d4
Ferdous110 <anjumferdous110@gmail.com>
2025-06-21 12:30:28 +0600

    add listing details

--- app.js
+++ app.js
@@ -18,16 +18,28 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended: true}));
  
 app.get ("/", (req, res) => {
     res.send("hi i am root");
 });
 
+// Index Route
 
 app.get("/Listings", async (req, res) => {
     const allListing = await Listing.find({});
     res.render("./listings/index.ejs", {allListing});
 });
+
+// Show Route 
+
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+});
+
+
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = Listing({
