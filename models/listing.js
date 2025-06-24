7ace260ca32ba7c198754f37619cac8e490cea5f
Ferdous110 <anjumferdous110@gmail.com>
2025-06-24 22:11:41 +0600

    add style in new  page

--- models/listing.js
+++ models/listing.js
@@ -2,29 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: String,
-    image: {
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
     filename: String,
     url: String,
     //  default: "",
     //     type: String,
     //    set: (v) => v === "" ? "" : v,
   },
-   
-        
-    //    // default: "https://unsplash.com/photos/mountains-stand-out-against-a-colorful-sunset-pzbm-wp8fa0",
-    //     type: String,
-    //     set: (v) => v === "" ? "" : v,
-    // },
-    price: Number,
-    location: String,
-    country: String,
-    
 
+  //    // default: "https://unsplash.com/photos/mountains-stand-out-against-a-colorful-sunset-pzbm-wp8fa0",
+  //     type: String,
+  //     set: (v) => v === "" ? "" : v,
+  // },
+  price: Number,
+  location: String,
+  country: String,
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
