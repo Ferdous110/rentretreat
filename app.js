acf369f50ce4fd570f144178efcd4cfe3a65d7b5
Ferdous110 <anjumferdous110@gmail.com>
2025-06-29 18:12:39 +0600

    error handling

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.get("/", (req, res) => {
 
 // Index Route
 
-app.get("/listings", async (req, res) => {
+app.get("/listings", warpAsync(async (req, res) => {
   const allListing = await Listing.find({});
-  res.render("./listings/index.ejs", { allListing });
-});
+  res.render("listings/index.ejs", { allListing });
+}));
 
 // New Route
 
@@ -48,11 +48,11 @@ app.get("/listings/new", (req, res) => {
 
 // Show Route
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", warpAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
-});
+}));
 
 // create Route
 
@@ -64,27 +64,27 @@ app.post("/listings", warpAsync(async (req, res, next) => {
 })
 );
 // Edit  Router
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", warpAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit.ejs", { listing });
-});
+}));
 
 // Update Route
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", warpAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 // Delete Route
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id", warpAsync(async (req, res) => {
   let { id } = req.params;
   let deletedlisting = await Listing.findByIdAndDelete(id);
   res.redirect("/listings");
-});
+}));
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = Listing({
@@ -100,13 +100,14 @@ app.delete("/listings/:id", async (req, res) => {
 
 // });
 
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"))
-});
+// app.all("*", (req, res, next) => {
+//   next(new ExpressError(404, "Page Not Found"))
+// });
 
 app.use((err, req, res, next) => {
-  let {statusCode, message} = err;
-  res.status(statusCode).send(message);
+  let {statusCode = 500, message = "something went wrong!"} = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.status(statusCode).send(message);
 });
 
 
