00d0eab7bba62a0f1414a0335ec2999e6422d721
Ferdous110 <anjumferdous110@gmail.com>
2025-07-02 22:57:49 +0600

    update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const warpAsync = require("./utils/warpAsync.js");
-const ExpressError = require("./utils/expressError");
+//const ExpressError = require("./utils/ExpressError");
 const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/rentRetreat";
